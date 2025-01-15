e787f10019936eb874dc0d1e199f142ab8e802d0
Virat Srivastava <vsrivastava.dev@gmail.com>
2025-01-16 00:26:27 +0530

    node setup

--- server.cjs
+++ server.cjs
@@ -4,6 +4,10 @@ const http = require('http')
 const {Server} = require('socket.io')
 const server  = http.createServer(app)
 const io = new Server(server)
+io.on('connection',(socket)=>{
+    console.log('socket connected',socket.id)
+})
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080 
+server.listen(PORT, () => console.log(`listening on PORT ${PORT}`))
